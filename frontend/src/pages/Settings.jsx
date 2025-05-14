92368a6e46010d589ffe586dfeea9f753fa6a5aa
wakana <2312110232@kindai.ac.jp>
2025-05-14 20:12:39 +0900

    ページ遷移用以外のボタン設置

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -4,7 +4,16 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <>
+    <div className="min-h-screen bg-white">
+      <div className="flex flex-col gap-15 items-center pt-45">
+        {/* 使い方 */}
+        <button className="rounded-full bg-blue text-white py-4 w-2/6 font-kiwi-maru hover:bg-blue-dark">使い方</button>
+        {/* ログアウト */}
+        <button className="rounded-full  bg-blue text-white  py-4  w-2/6 font-kiwi-maru hover:bg-blue-dark">ログイン</button>
+      </div>
+    </div>
+    </>
   )
 }
 
