b00da847483b7ce21418e89053015d1318c7e681
wakana <2312110232@kindai.ac.jp>
2025-05-14 17:54:26 +0900

    ボタンからページ遷移できるようにしたよ

--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -1,8 +1,12 @@
 // スタートページ
 
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 const Start = () => {
+
+  const navigate = useNavigate()
+
   return (
     <>
     <div className="min-h-screen bg-white">
@@ -11,9 +15,9 @@ const Start = () => {
 
         <div className="flex gap-8 justify-center mt-20">
           {/* ログインボタン */}
-         <button className="rounded-full bg-blue text-white px-6 py-2 font-kiwi-maru hover:bg-blue-dark">ログイン</button> 
+         <button onClick={() => navigate('/login')} className="rounded-full bg-blue text-white px-6 py-2 font-kiwi-maru hover:bg-blue-dark">ログイン</button> 
           {/* 新規登録ボタン */}
-         <button className="rounded-full  bg-blue text-white px-6 py-2 font-kiwi-maru hover:bg-blue-dark">新規登録</button>
+         <button onClick={() => navigate('/signup')} className="rounded-full  bg-blue text-white px-6 py-2 font-kiwi-maru hover:bg-blue-dark">新規登録</button>
         </div>
 
       {/* 説明 */}
