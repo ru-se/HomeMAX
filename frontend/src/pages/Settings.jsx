73cf969a53775aeb9c550c1cb7ece78859132ca2
wakana <2312110232@kindai.ac.jp>
2025-05-15 13:38:02 +0900

    使い方をボタンでページ遷移じゃなくてこの画面で書くことにしました

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -5,12 +5,12 @@ import React from 'react'
 const Settings = () => {
   return (
     <>
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white font-kiwi-maru">
       <div className="flex flex-col gap-15 items-center pt-45">
         {/* 使い方 */}
-        <button className="rounded-full bg-blue text-white py-4 w-2/6 font-kiwi-maru hover:bg-blue-dark">使い方</button>
+        <p className="w-1/3 text-center">使い方の文章</p>
         {/* ログアウト */}
-        <button className="rounded-full  bg-blue text-white  py-4  w-2/6 font-kiwi-maru hover:bg-blue-dark">ログイン</button>
+        <button className="rounded-full  bg-blue text-white  py-4  w-1/4 hover:bg-blue-dark">ログイン</button>
       </div>
     </div>
     </>
