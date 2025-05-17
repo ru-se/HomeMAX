d1752795ca8361d53e77c4ec8225f33fecad218c
Koseeee-27 <kosekose2741@gmail.com>
2025-05-18 05:46:37 +0900

    ほめマックスをなでて通知

--- frontend/src/features/Home/HomemaxImage.jsx
+++ frontend/src/features/Home/HomemaxImage.jsx
@@ -1,6 +1,6 @@
 // ほめマックスの画像
 
-import React from 'react'
+import React, { useState } from 'react'
 import homemaxImg1 from '../../assets/homemax_01-2.png';
 import homemaxImg2 from '../../assets/homemax_02.png';
 import homemaxImg3 from '../../assets/homemax_03.png';
@@ -8,6 +8,8 @@ import homemaxImg4 from '../../assets/homemax_04.png';
 import homemaxImg5 from '../../assets/homemax_05.png';
 import homemaxImg6 from '../../assets/homemax_06.png';
 import homemaxImg7 from '../../assets/homemax_07.png';
+import { toast } from 'react-toastify'
+
 const HomemaxImage = ({ OKCount }) => {
 
   // ほめマックスの画像を配列に格納
@@ -24,11 +26,29 @@ const HomemaxImage = ({ OKCount }) => {
   // OKCount に基づいて画像を選択
   const selectedImage = images[OKCount] 
   
+  const [hoverCount, setHoverCount] = useState(0)
+  const [notified, setNotified] = useState(false)
+
+  const handleMouseEnter = () => {
+    setHoverCount(prev => {
+      const newCount = prev + 1
+      if (newCount >= 6 && !notified) {
+        toast('撫でてくれてありがとう！すごい！！✨')
+        setNotified(true)
+      }
+      return newCount
+    })
+  }
 
   return (
     <div>
       <div>
-        <img src={selectedImage} alt="homemax" />
+        <img 
+          src={selectedImage} 
+          alt="homemax" 
+          onMouseEnter={handleMouseEnter}
+          className='cursor-pointer'
+        />
       </div>
     </div>
   )
