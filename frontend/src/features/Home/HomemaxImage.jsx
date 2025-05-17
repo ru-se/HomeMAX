c407523e263039fde7bd9b90e9703d0cf871f615
Koseeee-27 <kosekose2741@gmail.com>
2025-05-17 19:05:55 +0900

    ホーム画面復活！！！！！！！、メニューボタン配置、通知機能、吹き出しcss

--- frontend/src/features/Home/HomemaxImage.jsx
+++ frontend/src/features/Home/HomemaxImage.jsx
@@ -1,13 +1,34 @@
 // ほめマックスの画像
 
 import React from 'react'
-import homemaxImg from '../../assets/homemax_01.png'; // ← 画像をインポート
+import homemaxImg1 from '../../assets/homemax_01-2.png';
+import homemaxImg2 from '../../assets/homemax_02.png';
+import homemaxImg3 from '../../assets/homemax_03.png';
+import homemaxImg4 from '../../assets/homemax_04.png';
+import homemaxImg5 from '../../assets/homemax_05.png';
+import homemaxImg6 from '../../assets/homemax_06.png';
+import homemaxImg7 from '../../assets/homemax_07.png';
+const HomemaxImage = ({ OKCount }) => {
 
-const HomemaxImage = () => {
+  // ほめマックスの画像を配列に格納
+  const images = [
+    homemaxImg1,
+    homemaxImg2,
+    homemaxImg3,
+    homemaxImg4,
+    homemaxImg5,
+    homemaxImg6,
+    homemaxImg7,
+  ]
+
+  // OKCount に基づいて画像を選択
+  const selectedImage = images[OKCount] 
+  
+
   return (
     <div>
       <div>
-        <img src={homemaxImg} alt="homemax" />
+        <img src={selectedImage} alt="homemax" />
       </div>
     </div>
   )
--- frontend/src/features/Home/UserMessageBubble.jsx
+++ frontend/src/features/Home/UserMessageBubble.jsx
@@ -4,10 +4,16 @@ import React from 'react'
 
 const UserMessageBubble = ({ message }) => {
   return (
-    <div className='w-48 h-48 border-2 border-solid'>
-      <div>
-        {message}
+    <div className='mt-10 mr-20 flex-col-reverse '>
+      <div className='
+        relative w-90 h-85  px-4 py-2 text-base border-2  bg-pink shadow-xl rounded-full
+      '>
+        <div className='mt-12  w-64 mx-auto max-h-56 overflow-y-auto items-center text-center'>
+          {message}
+        </div>
       </div>
+      <div className="ml-10 w-20 h-20  px-4 py-2 text-base border-2  bg-pink shadow-xl rounded-full"></div>
+      <div className="w-10 h-10  px-4 py-2 text-base border-2  bg-pink shadow-xl rounded-full"></div>
     </div>
   )
 }
