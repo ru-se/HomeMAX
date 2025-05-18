bdd6deab00a20dee3354b0e756e9771157e69a65
Koseeee-27 <kosekose2741@gmail.com>
2025-05-18 12:47:30 +0900

    発表前調整完了、ごめんねタイマーいつもありがとう機能実装

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,14 +1,12 @@
 // 設定ページ
 
 import React from 'react'
-import Menu from '../components/menu/menu'
 import SettingsPage from '../features/Settings/SettingsPage'
 
 
 const Settings = () => {
   return (
     <SettingsPage />
-    // <Menu />
   )
 }
 
