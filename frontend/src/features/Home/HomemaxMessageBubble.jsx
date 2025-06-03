6b94c6df4619a848f221b56485709db9d9d1823f
wakana <2312110232@kindai.ac.jp>
2025-06-03 15:03:49 +0900

    ホーム画面のデザイン修正

--- frontend/src/features/Home/HomemaxMessageBubble.jsx
+++ frontend/src/features/Home/HomemaxMessageBubble.jsx
@@ -8,15 +8,15 @@ const HomemaxMessageBubble = ({ message }) => {
     //     <p>{message || '生成中...'}</p>
     //   </div>
     // </div>
-    <div className='h-full'>
+    <div className='h-full flex items-center '>
       <div
         className='
-           w-90 h-120 max-h-9/10  border-2 border-solid relative  px-4 py-2 text-base font-normal  border-black rounded-4xl bg-pink
+          min-w-64 min-h-24 max-w-md max-h-96 border-2 border-solid relative  px-12 py-8 text-base font-normal  border-black rounded-4xl bg-pink
           before:absolute before:top-1/2 before:right-0 before:translate-x-full before:-translate-y-1/2
           before:border-y-[10px] before:border-l-[20px] before:border-y-transparent before:border-l-black
           after:absolute after:top-1/2 after:right-0 after:translate-x-full after:-translate-y-1/2
           after:border-y-[7.8px] after:border-l-[15.5px] after:border-y-transparent after:border-l-pink shadow-xl
-        '>
+          flex items-center justify-center'>
           <div className='w-auto max-h-full overflow-y-auto'>
             { message || '生成中...'}
           </div>
