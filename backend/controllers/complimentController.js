c2238d293d543714a800e7feb03dbfc9324b2da3
Koseeee-27 <kosekose2741@gmail.com>
2025-11-03 22:53:41 +0900

    デフォルトタイトルを「ほめマックスより」から「頑張った君へ」に変更

--- backend/controllers/complimentController.js
+++ backend/controllers/complimentController.js
@@ -44,7 +44,7 @@ exports.generateCompliment = async (req, res) => {
 
         const parts = rawResponse.split('\n---\n');
 
-        let titleText = "ほめマックスより"; // デフォルトタイトル
+        let titleText = "頑張った君へ"; // デフォルトタイトル
         let complimentText = "";
 
         if (parts.length >= 2) {
